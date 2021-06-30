c793539fb5bbc9754bb54242f961db07c6ef1bd6
Katharina Schuster <wi20b029@technikum-wien.at>
2021-06-30 10:27:48 +0200

    2. Test korrigiert

--- src/test/java/at/schuster/TestUptimeControllerTest.java
+++ src/test/java/at/schuster/TestUptimeControllerTest.java
@@ -21,19 +21,19 @@ public class TestUptimeControllerTest {
     }
 
 
-//    @Test
-//    public void test_calculateUptime_fix(){
-//
-//        // Arrange // 1. Test ohne Eingabe in controller über die API
-//        TestUptimeController controller = new TestUptimeController();
-//
-//        // Act
-//        String actualValue = controller.calculateUptime();
-//        // Assert
-//        Assertions.assertEquals(String.format("43178,40"), actualValue); //verwendet nach dem Build Fehler in den Actions
-//        //Assertions.assertEquals("43178,40", actualValue);
+    @Test
+    public void test_calculateUptime_fix(){
 
- //   }
+        // Arrange // 1. Test ohne Eingabe in controller über die API
+        TestUptimeController controller = new TestUptimeController();
+
+        // Act
+        String actualValue = controller.calculateUptime();
+        // Assert
+        Assertions.assertEquals(String.format("%.2f", 43178.40), actualValue); //verwendet nach dem Build Fehler in den Actions
+        //Assertions.assertEquals("43178,40", actualValue);
+
+    }
 
 }
 
